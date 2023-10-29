4710f1444077a16ca697dc1ff09013534bb1ddd5
jamshid0705 <jamshidshamshod0705@gmail.com>
2023-10-29 17:22:42 +0500

    types

--- index.ts
+++ index.ts
@@ -3,13 +3,17 @@ const url='https://jsonplaceholder.typicode.com/todos/1'
 interface todo{
   id:number,
   title:string,
-  completed:string
+  completed:boolean
 }
 axios.get(url).then(response=>{
   const data=response.data as todo
-  console.log(`
-    Data id ${data.id}
-    data title ${data.title}
-    data completed ${data.completed}`
-  )
+  todo(data.id,data.title,data.completed)
 })
+
+const todo=(id:number,title:string,completed:boolean)=>{
+  console.log(`
+    Data id: ${id}
+    data title: ${title}
+    data completed: ${completed}`
+  )
+}
